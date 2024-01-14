8777c071eeaaaaf4637a853d32d0d287e24f139a
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-01-14 12:44:09 +0330

    feat: implemented cart and price format

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,52 @@
+"use client";
+import { ShoppingCart } from "lucide-react";
+import { SheetTrigger, Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+
 const Cart = () => {
-   return <div>Card</div>;
+   const itemCount = 1;
+   return (
+      <Sheet>
+         <SheetTrigger className="group -m-2 flex items-center p-2">
+            <ShoppingCart
+               aria-hidden="true"
+               className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+            />
+            <span className="ml-2 text-sm font-medium text-gray-600 group-hover:text-gray-800">
+               0
+            </span>
+         </SheetTrigger>
+         <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+            <SheetHeader className="space-y-2.5 pr-6">
+               <SheetTitle>Cart (0)</SheetTitle>
+            </SheetHeader>
+            {itemCount > 0 ? (
+               <>
+                  <div className="flex flex-col w-full pr-6">
+                     {/* TODO: Cart logic */}
+                     cart items
+                  </div>
+                  <div className="space-y-4 pr-6">
+                     <Separator />
+                     <div className="space-y-1.5 text-sm">
+                        <div className="flex">
+                           <span className="flex-1">Shipping</span>
+                           <span>Free</span>
+                        </div>
+                        <div className="flex">
+                           <span className="flex-1">Transaction Fee</span>
+                           <span>{formatPrice(0.1)}</span>
+                        </div>
+                     </div>
+                  </div>
+               </>
+            ) : (
+               <div className=""></div>
+            )}
+         </SheetContent>
+      </Sheet>
+   );
 };
 
 export default Cart;
