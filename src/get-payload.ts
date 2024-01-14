7ed6788de43a6c9e05d05a427355cbe90baf17e2
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-01-14 15:02:03 +0330

    pkg: install payload

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,3 +1,15 @@
 import dotenv from "dotenv";
+import path from "path";
 
+dotenv.config({
+   path: path.resolve(__dirname, "../.env"),
+});
+
+let cached = (global as any).payload;
+if (!cached) {
+   cached = (global as any).payload = {
+      client: null,
+      promise: null,
+   };
+}
 export const getPayLoadClient = async () => {};
