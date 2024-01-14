b47ae67b7786fe2b19898b6f05078db91710d443
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-01-14 15:31:23 +0330

    feat: initial getPayLoadClient fun for every where access to database

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
+import payload from "payload";
+import type { InitOptions } from "payload/config";
 
 dotenv.config({
    path: path.resolve(__dirname, "../.env"),
@@ -12,4 +14,29 @@ if (!cached) {
       promise: null,
    };
 }
-export const getPayLoadClient = async () => {};
+
+interface Args {
+   initOptions?: Partial<InitOptions>;
+}
+export const getPayLoadClient = async ({ initOptions }: Args = {}) => {
+   if (!process.env.PAYLOAD_SECRET) {
+      throw new Error("PAYLOAD_SECRET is missing");
+   }
+   if (cached.client) {
+      return cached.client;
+   }
+   if (!cached.promise) {
+      cached.promise = payload.init({
+         secret: process.env.PAYLOAD_SECRET,
+         local: initOptions?.express ? false : true,
+         ...(initOptions || {}),
+      });
+   }
+   try {
+      cached.client = await cached.promise;
+   } catch (error: unknown) {
+      cached.promise = null;
+      throw error;
+   }
+   return cached.client;
+};
