79ed25c7e6390adf061e8b623e1e6697a6620dae
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-01-18 18:03:12 +0330

    chore: get user from payload endpoint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,12 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
+import { cookies } from "next/headers";
+import { getServerSideUser } from "@/lib/payload-utils";
 
-const Navbar = () => {
-   const user = null;
+const Navbar = async () => {
+   const nextCookies = cookies();
+   const { user } = await getServerSideUser(nextCookies);
    return (
       <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
          <header className="relative bg-white">
