e6bb18bb0203e9d150d8ef136de68424977beb2e
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-01-28 12:53:10 +0330

    chore: start to creating the user checkout

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,11 +1,26 @@
 import { z } from "zod";
 import { privateProcedure, publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { getPayLoadClient } from "../get-payload";
 
 export const paymentRouter = router({
    createSession: privateProcedure
       .input(z.object({ productIds: z.array(z.string()) }))
-      .mutation(({ ctx, input }) => {
+      .mutation(async ({ ctx, input }) => {
          const { user } = ctx;
          let { productIds } = input;
+         if (productIds.length === 0) {
+            throw new TRPCError({ code: "BAD_REQUEST" });
+         }
+
+         const payload = await getPayLoadClient();
+         const { docs: products } = await payload.find({
+            collection: "products",
+            where: {
+               id: {
+                  in: productIds,
+               },
+            },
+         });
       }),
 });
